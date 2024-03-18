7dbe5ab379a13ec56bf4477cbaf0434e35f13264
Sheryl Chee <sherylmichaela@gmail.com>
2024-03-18 23:58:23 +1100

    Edited Navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import "./App.css";
+import Navbar from "./Components/Navbar";
 
 export default function App() {
   return (
     <>
-      <h1>Ecommerce Website</h1>
+      <Navbar />
     </>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,24 +1,26 @@
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-import NavDropdown from "react-bootstrap/NavDropdown";
+// import NavDropdown from "react-bootstrap/NavDropdown";
 
 export default function BasicExample() {
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container>
-        <Navbar.Brand href="#home">Sheryl's Shop</Navbar.Brand>
+        <Navbar.Brand href="/">Sheryl's Shop</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <NavDropdown title="Products" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+            {/* <NavDropdown title="Products" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Shop by Category</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">
                 Another action
               </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-            </NavDropdown>
-            <Nav.Link href="#link">Contact</Nav.Link>
+            </NavDropdown> */}
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="products">Products</Nav.Link>
+            <Nav.Link href="contact">Contact</Nav.Link>
+            <Nav.Link href="cart">Cart</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
