5daad2cb173a21441ff8d0333278ee5dc275301f
Sheryl Chee <sherylmichaela@gmail.com>
2024-03-23 02:35:46 +1100

    Completed cart page

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -4,12 +4,14 @@
 import Container from "react-bootstrap/Container";
 import Button from "react-bootstrap/Button";
 import Table from "react-bootstrap/Table";
-// import Form from "react-bootstrap/Form";
+import Form from "react-bootstrap/Form";
 import { useCart } from "react-use-cart";
 
 export default function Cart() {
   const { isEmpty, totalUniqueItems, items, updateItemQuantity, removeItem } =
     useCart();
+
+  const { cartTotal } = useCart();
 
   if (isEmpty)
     return (
@@ -27,74 +29,83 @@ export default function Cart() {
 
   return (
     <Container>
-      <div className="col-12">
-        <h1 style={{ marginTop: 100, marginBottom: 50 }}>
-          Cart ({totalUniqueItems})
-        </h1>
-        <Table className="text-center align-middle">
-          <thead>
-            <tr>
-              <th>Product Image</th>
-              <th>Product Name</th>
-              <th>Price</th>
-              <th>Quantity</th>
-              <th></th>
-            </tr>
-          </thead>
-          <tbody>
-            {items.map((item, index) => {
-              return (
-                <tr key={index}>
-                  <td>
-                    <img
-                      src={item.image}
-                      alt="product-img"
-                      style={{ height: "6rem" }}
-                    />
-                  </td>
-                  <td>{item.name}</td>
-                  <td>${item.price}</td>
-                  <td>
-                    <Button
-                      variant="primary"
-                      style={{ marginRight: 10 }}
-                      onClick={() =>
-                        updateItemQuantity(item.id, item.quantity - 1)
-                      }
-                    >
-                      -
-                    </Button>
-                    {/* <Form.Control
-                      type="text"
-                      placeholder={item.quantity}
-                      style={{ width: 50 }}
-                      readOnly
-                    /> */}
-                    {item.quantity}
-                    <Button
-                      variant="primary"
-                      style={{ marginLeft: 10 }}
-                      onClick={() =>
-                        updateItemQuantity(item.id, item.quantity + 1)
-                      }
-                    >
-                      +
-                    </Button>
-                  </td>
-                  <td>
-                    <Button
-                      variant="danger"
-                      className="ms-2"
-                      onClick={() => removeItem(item.id)}
-                    >
-                      Remove
-                    </Button>
-                  </td>
-                </tr>
-              );
-            })}
-          </tbody>
-        </Table>
+      <h1 style={{ marginTop: 100, marginBottom: 50 }}>
+        Cart ({totalUniqueItems})
+      </h1>
+      <Table className="text-center align-middle">
+        <thead>
+          <tr>
+            <th>Product Image</th>
+            <th>Product Name</th>
+            <th>Price</th>
+
+            <th>Quantity</th>
+
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          {items.map((item, index) => {
+            return (
+              <tr key={index}>
+                <td>
+                  <img
+                    src={item.image}
+                    alt="product-img"
+                    style={{ height: "6rem" }}
+                  />
+                </td>
+                <td>{item.name}</td>
+                <td>${item.price}</td>
+                <td>
+                  <Button
+                    variant="primary"
+                    style={{ marginRight: 15, marginBottom: 6 }}
+                    onClick={() =>
+                      updateItemQuantity(item.id, item.quantity - 1)
+                    }
+                  >
+                    -
+                  </Button>
+
+                  <Form.Control
+                    type="text"
+                    value={item.quantity}
+                    style={{ width: "50px" }}
+                    className="d-inline-block text-center"
+                    readOnly
+                  />
+
+                  <Button
+                    variant="primary"
+                    style={{ marginLeft: 15, marginBottom: 6 }}
+                    onClick={() =>
+                      updateItemQuantity(item.id, item.quantity + 1)
+                    }
+                  >
+                    +
+                  </Button>
+                </td>
+                <td>
+                  <Button
+                    variant="danger"
+                    className="ms-2"
+                    onClick={() => removeItem(item.id)}
+                  >
+                    Remove
+                  </Button>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </Table>
+      <div style={{ textAlign: "right" }}>
+        <h2>Total: ${cartTotal}</h2>
+        <br />
+        <Button variant="primary" style={{ marginTop: "-20px" }}>
+          Click to Checkout
+        </Button>
       </div>
     </Container>
   );
