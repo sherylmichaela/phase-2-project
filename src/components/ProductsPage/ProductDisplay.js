85b8f1c8cbda27020ce08bbd6c2612741369ff02
Sheryl Chee <sherylmichaela@gmail.com>
2024-03-26 13:59:35 +1100

    Finished up product display page

--- src/components/ProductsPage/ProductDisplay.js
+++ src/components/ProductsPage/ProductDisplay.js
@@ -1,11 +1,20 @@
-import Container from "react-bootstrap/Container";
+// import Container from "react-bootstrap/Container";
+// import Row from "react-bootstrap/Row";
+// import Col from "react-bootstrap/Col";
+import Button from "react-bootstrap/Button";
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { products } from "../../data.js";
+import { useCart } from "react-use-cart";
+import "./ProductDisplay.css";
 
 export default function ProductDisplay() {
   const { productId } = useParams();
   const [product, setProduct] = useState(null);
+
+  const [buttonText, setButtonText] = useState("Add To Cart");
+  const [buttonDisabled, setButtonDisabled] = useState(false);
+  const { addItem } = useCart();
 
   useEffect(() => {
     const productFound = products.find(
@@ -15,16 +24,43 @@ export default function ProductDisplay() {
     setProduct(productFound);
   }, [productId]);
 
+  const handleClick = () => {
+    // Changing button text and state when clicked
+    setButtonText("Added to Cart!");
+    setButtonDisabled(true);
+
+    setTimeout(() => {
+      setButtonText("Add to Cart");
+    }, 1000);
+
+    setTimeout(() => {
+      setButtonDisabled(false);
+    }, 1000);
+
+    return addItem(product);
+  };
+
   return (
-    <Container>
+    <>
       {product ? (
-        <>
-          <h4 style={{ marginTop: 100 }}>{product.name}</h4>
-          <h3>{product.description}</h3>
-        </>
+        <div className="product-page">
+          <div className="product-image-container">
+            <img src={product.image} alt={product.name} />
+            {/* <img src={product.image} alt="Product Image" /> */}
+          </div>
+          <div className="product-details">
+            <h2>{product.name}</h2>
+            <h4>${product.price}</h4>
+            <p>{product.description}</p>
+
+            <Button disabled={buttonDisabled} onClick={handleClick}>
+              {buttonText}
+            </Button>
+          </div>
+        </div>
       ) : (
         <p style={{ marginTop: 100 }}>Nothing found</p>
       )}
-    </Container>
+    </>
   );
 }
